6d570f5768453eadc485e01e76eab71d908a9d95
Andrey Prohorov <aprohorov@changers.team>
2017-02-25 17:25:33 +0300

    added new div via hello.js

--- hello.js
+++ hello.js
@@ -4,4 +4,8 @@ function sayHi() {
     var newHeader = goog.dom.createDom('h1', {'style': 'background-color:#EEE'},
         'Hello world!');
     goog.dom.appendChild(document.body, newHeader);
+
+    var newDiv = goog.dom.createDom('div', {'style': 'background-color:#EEE'},
+        'I am your new DIV!');
+    goog.dom.appendChild(document.body, newDiv);
 }
